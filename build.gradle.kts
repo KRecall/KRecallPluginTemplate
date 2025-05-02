8857ece03da582b67a1e658edc83259c9c641e37
octest <octest8848@qq.com>
2025-05-02 12:29:03 +0800

    build: 更新 krecall plugins basiclib 版本
    - 将 io.github.octestx.krecall.plugins.basiclib:library 版本从1.4.8 升级到 1.4.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(compose.material3)
     implementation(compose.components.resources)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("io.github.octestx.krecall.plugins.basiclib:library:1.4.8")
+    implementation("io.github.octestx.krecall.plugins.basiclib:library:1.4.9")
     implementation("io.github.octestx:basic-multiplatform-lib:0.1.1")
     implementation("io.github.octestx:basic-multiplatform-ui-lib:0.1.3")
 }
