0a78de5f455c3cf1bbf25b9cffbd7e6cd60f98af
octest <octest8848@qq.com>
2025-05-03 13:24:55 +0800

    build(gradle): 优化项目构建和依赖打包配置
    - 创建自定义配置 PluginDependenceBundle，用于指定需要打包的依赖
    - 修改 shadowJar 任务，仅包含自定义配置的依赖和编译输出
    - 添加资源文件和自定义元数据的打包配置
    - 新增 implementationAndPackage 函数，简化依赖添加操作
    - 优化依赖版本号引用，提高可维护性

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 val groupName = "io.github.octestx.krecall.plugins.captureScreen.kdespectacle"
 val versionName = "1.0-SNAPSHOT"
 val pluginPackName = "CaptureScreenByKDESpectaclePlugin"
@@ -34,6 +32,11 @@ repositories {
     google()
 }
 
+// 创建自定义配置用于指定需要打包的依赖
+configurations {
+    create("PluginDependenceBundle")
+}
+
 dependencies {
     // Note, if you develop a library, you should use compose.desktop.common.
     // compose.desktop.currentOs should be used in launcher-sourceSet
@@ -46,6 +49,9 @@ dependencies {
     implementation("io.github.octestx.krecall.plugins.basiclib:library:1.4.9")
     implementation("io.github.octestx:basic-multiplatform-lib:0.1.1")
     implementation("io.github.octestx:basic-multiplatform-ui-lib:0.1.3")
+
+    //用shadowJar打包时不会打包任何依赖，如果需要打包依赖, 请使用以下方式添加依赖
+//    implementationAndPackage("com.aallam.openai:openai-client:4.0.0")
 }
 
 //compose.desktop {
@@ -67,12 +73,28 @@ tasks {
         archiveVersion.set(versionName)
         archiveClassifier.set("")
 
-        // 包含所有插件元数据
+        // 只包含自定义配置的依赖
+        configurations = listOf(project.configurations["PluginDependenceBundle"])
+
+        // 包含编译输出
+        from(sourceSets.main.get().output)
+
+        // 包含资源文件
+        from(sourceSets.main.get().resources)
+
+        // 包含自定义元数据
         into("META-INF/plugins") {
             from(generatePluginMetadata())
         }
 
-        configurations = listOf(project.configurations.runtimeClasspath.get())
+        // 资源过滤配置
+        exclude("META-INF/*.SF")
+        exclude("META-INF/*.DSA")
+        exclude("META-INF/*.RSA")
+        exclude("META-INF/NOTICE*")
+        exclude("META-INF/LICENSE*")
+        exclude("META-INF/INDEX.LIST")
+        exclude("**/module-info.class")
     }
 
     register("generateMetadata") {
@@ -107,6 +129,11 @@ fun generatePluginMetadata(): FileCollection {
         }
     }
     return files(fileTree("build/metadata").files)
+}
+
+fun DependencyHandlerScope.implementationAndPackage(dependency: Any) {
+    implementation(dependency)
+    "PluginDependenceBundle"(dependency)
 }
 
 enum class OS { WIN, MACOS, LINUX, OTHER }
